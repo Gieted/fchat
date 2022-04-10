c2fee550d346c9d314a61def56d10be4122e2938
Paweł Kiełb <pawel.kielb@protonmail.com>
2022-04-10 14:32:35 +0200

    Limit message length

--- shared/src/main/java/pl/pawelkielb/fchat/data/Message.java
+++ shared/src/main/java/pl/pawelkielb/fchat/data/Message.java
@@ -8,5 +8,9 @@ public record Message(Name author, String content) {
         if (content.isBlank()) {
             throw new IllegalArgumentException("Content cannot be blank");
         }
+
+        if (content.length() > 10000) {
+            throw new IllegalArgumentException("A message content cannot be longer than 10000 characters");
+        }
     }
 }
