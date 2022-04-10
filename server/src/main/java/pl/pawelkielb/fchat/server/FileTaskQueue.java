a95e5e0b4693143b5560ece9ff9ed3436483d110
Paweł Kiełb <pawel.kielb@protonmail.com>
2022-04-10 14:01:39 +0200

    Write JavaDoc for the FileTaskQueue

--- server/src/main/java/pl/pawelkielb/fchat/server/FileTaskQueue.java
+++ server/src/main/java/pl/pawelkielb/fchat/server/FileTaskQueue.java
@@ -8,7 +8,108 @@ import java.util.function.Consumer;
 
 import static pl.pawelkielb.fchat.CollectionUtils.lastOrNull;
 
+
+/**
+ * An advanced task queue designed for working with files. It allows segregating tasks by a key.
+ * Two tasks can run in parallel if they have a different keys.
+ * In addition, all the tasks are also either "reading" or "writing". Two or more reading tasks can run at the same time,
+ * but only one writing task can be running at the same time (for the given key).
+ * Also reading and writing tasks cannot run at the same time (for the given key). It's thread-safe.
+ *
+ * @param <K> a type of the key
+ */
 public class FileTaskQueue<K> {
+    private final Map<K, List<Layer>> layersByKey = new HashMap<>();
+    private final TaskQueue masterQueue = new TaskQueue();
+
+    /**
+     * Schedules a new suspending, reading task to run.
+     * <p>
+     * e.g.
+     * <pre>
+     * {@code
+     * fileTaskQueue.runSuspendReading(clientId, task -> {
+     *     readFile().thenRun(task::complete);
+     * });
+     * }
+     * </pre>
+     *
+     * @param key a key of the task
+     * @param fn  a function to run
+     * @param <T> a type returned by the task
+     * @return A future resolving to a value returned by the task.
+     */
+    public <T> CompletableFuture<T> runSuspendReading(K key, Consumer<CompletableFuture<T>> fn) {
+        CompletableFuture<T> masterFuture = new CompletableFuture<>();
+
+        masterQueue.run(() -> {
+            List<Layer> layers = getLayers(key);
+
+            Layer lastLayer = lastOrNull(layers);
+            if (lastLayer == null || lastLayer instanceof WriteLayer) {
+                lastLayer = new ReadLayer();
+                layers.add(lastLayer);
+            }
+
+            ((ReadLayer) lastLayer).tasks.add(new Task<>(fn, masterFuture));
+
+            return layers;
+        }).thenAccept(this::processLayers);
+
+        return masterFuture;
+    }
+
+    /**
+     * Schedules a new suspending, writing task to run.
+     *
+     * @param key a key of the task
+     * @param fn  a function to run
+     * @param <T> a type returned by the task
+     * @return A value returned by the task
+     */
+    public <T> CompletableFuture<T> runSuspendWriting(K key, Consumer<CompletableFuture<T>> fn) {
+        CompletableFuture<T> masterFuture = new CompletableFuture<>();
+
+        masterQueue.run(() -> {
+            List<Layer> layers = getLayers(key);
+
+            WriteLayer writeLayer = new WriteLayer(new Task<>(fn, masterFuture));
+            layers.add(writeLayer);
+
+            return layers;
+        }).thenAccept(this::processLayers);
+
+        return masterFuture;
+    }
+
+    /**
+     * Schedules a new no-suspending, reading task to run.
+     *
+     * @param key a key of the task
+     * @param fn  a function to run
+     * @return A future that will resolve after the task completes.
+     */
+    public CompletableFuture<Void> runReading(K key, Runnable fn) {
+        return runSuspendReading(key, task -> {
+            fn.run();
+            task.complete(null);
+        });
+    }
+
+    /**
+     * Schedules a new no-suspending, writing task to run.
+     *
+     * @param key a key of the task
+     * @param fn  a function to run
+     * @return A future that will resolve after the task completes.
+     */
+    public CompletableFuture<Void> runWriting(K key, Runnable fn) {
+        return runSuspendWriting(key, task -> {
+            fn.run();
+            task.complete(null);
+        });
+    }
+
     private interface Layer {
     }
 
@@ -31,9 +132,6 @@ public class FileTaskQueue<K> {
 
     private record WriteLayer(Task<?> task) implements Layer {
     }
-
-    private final Map<K, List<Layer>> layersByKey = new HashMap<>();
-    private final TaskQueue masterQueue = new TaskQueue();
 
     private static <T> void processLayer_processTask(Task<T> task, List<CompletableFuture<?>> futures) {
         CompletableFuture<T> future = task.future;
@@ -86,54 +184,5 @@ public class FileTaskQueue<K> {
         layersByKey.entrySet().removeIf(entry -> entry.getKey() != key && entry.getValue().isEmpty());
 
         return layersByKey.computeIfAbsent(key, k -> new LinkedList<>());
-    }
-
-    public <T> CompletableFuture<T> runSuspendReading(K key, Consumer<CompletableFuture<T>> fn) {
-        CompletableFuture<T> masterFuture = new CompletableFuture<>();
-
-        masterQueue.run(() -> {
-            List<Layer> layers = getLayers(key);
-
-            Layer lastLayer = lastOrNull(layers);
-            if (lastLayer == null || lastLayer instanceof WriteLayer) {
-                lastLayer = new ReadLayer();
-                layers.add(lastLayer);
-            }
-
-            ((ReadLayer) lastLayer).tasks.add(new Task<>(fn, masterFuture));
-
-            return layers;
-        }).thenAccept(this::processLayers);
-
-        return masterFuture;
-    }
-
-    public <T> CompletableFuture<T> runSuspendWriting(K key, Consumer<CompletableFuture<T>> fn) {
-        CompletableFuture<T> masterFuture = new CompletableFuture<>();
-
-        masterQueue.run(() -> {
-            List<Layer> layers = getLayers(key);
-
-            WriteLayer writeLayer = new WriteLayer(new Task<>(fn, masterFuture));
-            layers.add(writeLayer);
-
-            return layers;
-        }).thenAccept(this::processLayers);
-
-        return masterFuture;
-    }
-
-    public CompletableFuture<Void> runReading(K key, Runnable fn) {
-        return runSuspendReading(key, task -> {
-            fn.run();
-            task.complete(null);
-        });
-    }
-
-    public CompletableFuture<Void> runWriting(K key, Runnable fn) {
-        return runSuspendWriting(key, task -> {
-            fn.run();
-            task.complete(null);
-        });
     }
 }
